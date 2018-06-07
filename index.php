cf0762fb78e9250dd09ea55821d16a428821e901
Lucien <lucien@lucien.ink>
2018-06-08 01:18:07 +0800

    0.1.1

--- index.php
+++ index.php
@@ -1,47 +1,30 @@
 <?php
-function genTextarea($name, $value, $width, $height, $placeholder = '') {
-    $html = "<textarea name='{$name}' " . "style='width:{$width}; height:{$height} ' ";
-    if (isset($value) && !empty($value)) $html .= ">{$value}</textarea>";
-    else if ('' != $placeholder) $html .= "placeholder='{$placeholder}'></textarea>";
-    else $html .= "></textarea>";
-    return $html;
+function genTextarea($name, $args='', $value='') {
+    return "<textarea name='{$name}' {$args}" . ">{$value}</textarea>";
 }
 
-function combine($arr) {
-	$ret = "";
-	foreach ($arr as $line) $ret = $ret . $line;
-	return $ret;
+function str_combine($arr) {
+    $ret = "";
+    foreach ($arr as $line) $ret = $ret . $line;
+    return $ret;
 }
-
-function homePage() {
-    $hostname = $_SERVER['HTTP_HOST'];
-    $html = combine(file('./homePage.html'));
-    return str_replace('{&hostname}', $hostname, $html);
-}
-
-function editpage($file_path) {
-	$pre = "<html><head><title>NetClip</title></head><form action='./submit.php' method='post'><div>";
-	$mid = "<textarea name='file_path' style='display: none'>{$file_path}</textarea>" . genTextarea("text", "Input something there, press \"Enter\" to submit.", "70%", "50%");
-    $suf = "</div><div><input type='submit' value='submit'/></div></form></html>";
-	return $pre . $mid . $suf;
-}
-
-function footer() {
-
-}
-
 
 $file_name = str_replace('/', '', $_SERVER["PATH_INFO"]);
-if ($file_name == "") echo homePage();
-else {
+if ($file_name == "") {
+    $hostname = $_SERVER['HTTP_HOST'];
+    $html = str_combine(file('./homePage.html'));
+    echo str_replace('{&hostname}', $hostname, $html);
+} else {
     $file_path = "./file/" . $file_name;
     if (file_exists($file_path)) {
-        echo genTextarea("output", combine(file($file_path)), "70%", "50%");
-      	echo "<div><a href='http://{$_SERVER['HTTP_HOST']}'>
-		  		<input type='button' title='Back to home' value='Back to home'/></a>";
+        $html = str_combine(file('./base.html'));
+        $tmp = "<b>The file shows below</b>" . genTextarea("output", "rows=10 readonly='readonly'", str_combine(file($file_path))) . "<a href='http://{$_SERVER['HTTP_HOST']}'><input type='button' title='Back to home' value='Back to home'/></a>";
+        echo str_replace('{&body}', $tmp, $html);
         unlink($file_path);
-    } else echo editpage($file_path);
+    } else {
+        $html = str_combine(file('./base.html'));
+        $tmp = "<b>This file is empty, you can input something below</b><form action='./submit.php' method='post'><textarea name='file_path' style='display: none'>{$file_path}</textarea>" . genTextarea("text", "rows=10") . "<input type='submit' value='submit'/></form>";
+        echo str_replace('{&body}', $tmp, $html);
+    }
 }
 ?>
-
-
--- submit.php
+++ submit.php
@@ -1,14 +1,22 @@
 <?php
+function str_combine($arr) {
+    $ret = "";
+    foreach ($arr as $line) $ret = $ret . $line;
+    return $ret;
+}
+
 $file_path = $_POST['file_path'];
 $text = $_POST['text'];
 if (!is_dir("./file")) mkdir("./file", 0755);
 $newfile = fopen($file_path, "w");
 fwrite($newfile, $text);
+$html = str_combine(file('./base.html'));
 if (file_exists($file_path)) {
-	echo "<div><p>Saved.</p></div>
-		  <div><a href='http://{$_SERVER['HTTP_HOST']}'>
+	$tmp = "<h2>Saved.</h2>
+		  <a href='http://{$_SERVER['HTTP_HOST']}'>
 		  		<input type='button' title='Back to home' value='Back to home'/></a>";
-}
-else echo "Failed.";
+	echo str_replace('{&body}', $tmp, $html);
+} else echo str_replace('{&body}', "<p>Failed.</p>", $html);
 ?>
+
 
