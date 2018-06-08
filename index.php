c6cb1b97341ddbda64b72df0c01e8c1472ef8699
Lucien <lucien@lucien.ink>
2018-06-08 12:49:39 +0800

    0.1.4

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
-function genTextarea($name, $args='', $value='') {
+function genTextarea($name, $args='', $value='') { // 生成一个textarea
     return "<textarea name='{$name}' {$args}" . ">{$value}</textarea>";
 }
 
-function str_combine($arr) {
+function str_combine($arr) { // 将一个string数组连接成一个string
     $ret = "";
     foreach ($arr as $line) $ret = $ret . $line;
     return $ret;
 }
 
-$file_name = str_replace('/', '', $_SERVER["PATH_INFO"]);
-if ($file_name == "") {
-    $hostname = $_SERVER['HTTP_HOST'];
-    $html = str_combine(file('./base.html'));
+$file_name = str_replace('/', '', $_SERVER["PATH_INFO"]); // 取当前路由的后缀
+if ($file_name == "") { // 如果没有后缀，那么显示主页
+    $hostname = $_SERVER['HTTP_HOST']; // 获取当前域名
+    $html = str_combine(file('./base.html')); // 取网页框架
     $tmp = "<h2>How to use</h2>
     <li><p>Enter any word behind this page's URL in browser and input anything you want.</p></li>
     <li><p>On any PC or Phone enter in the same URL to view the text you write just now.</p></li>
@@ -21,18 +21,20 @@ if ($file_name == "") {
     <p align='right'><a href='http://www.lucien.ink' target='_blank'>&copy; 2018 Lucien Shui</a></p>";
     echo str_replace('{&body}', $tmp, $html);
 } else {
-    $file_name = str_replace('.', 'dot', $file_name);
-    $file_path = "./.file/" . $file_name;
-    if (file_exists($file_path)) {
-        $html = str_combine(file('./base.html'));
-        $tmp = "<b>The file shows below</b>" . genTextarea("output", "rows=10 readonly='readonly'", str_combine(file($file_path))) . "<a href='http://{$_SERVER['HTTP_HOST']}'><input type='button' title='Back to home' value='Back to home'/></a>";
+    $file_name = str_replace('.', 'dot', $file_name); // 将"."全部替换
+    $file_path = "./.file/" . $file_name; // 文件路径
+    $host_name = $_SERVER['HTTP_HOST'];
+    if (file_exists($file_path)) { // 如果文件存在
+        $html = str_combine(file('./base.html')); // 取网页框架
+        $tmp = "<b>The file shows below</b>" . genTextarea("output", "rows=10 readonly='readonly'", str_combine(file($file_path))) . "<a href='http://{$host_name}'><input type='button' title='goback' value='Home'/></a>"; // 显示文本
         echo str_replace('{&body}', $tmp, $html);
         unlink($file_path);
-    } else {
-        $html = str_combine(file('./base.html'));
-        $tmp = "<b>This file is empty, you can input something below</b><form action='./submit.php' method='post'><textarea name='file_path' style='display: none'>{$file_path}</textarea>" . genTextarea("text", "rows=10") . "<input type='submit' value='Submit'/></form>";
+    } else { // 如果文件不存在
+        $html = str_combine(file('./base.html')); // 取网页框架
+        $tmp = "<b>This file is empty, you can input something below</b><form action='./submit.php' method='post'><textarea name='file_name' style='display: none'>{$file_name}</textarea>" . genTextarea("text", "rows=10") . "<input type='submit' value='Submit'/>"; // 创建表单
         echo str_replace('{&body}', $tmp, $html);
     }
 }
 ?>
+
 
--- submit.php
+++ submit.php
@@ -5,20 +5,30 @@ function str_combine($arr) {
     return $ret;
 }
 
-$file_path = $_POST['file_path'];
-if ($file_path == "") header("location:http://{$_SERVER['HTTP_HOST']}");
-$text = $_POST['text'];
-if (!is_dir("./.file")) mkdir("./.file", 0755);
-$newfile = fopen($file_path, "w");
-fwrite($newfile, $text);
-$html = str_combine(file('./base.html'));
-if (file_exists($file_path)) {
-	$tmp = "<h2>Saved.</h2>
-		  <a href='http://{$_SERVER['HTTP_HOST']}'>
-		  		<input type='button' title='Back to home' value='Back to home'/></a>";
-    echo "<meta http-equiv='refresh' content='3; url=http://{$_SERVER['HTTP_HOST']}";
-	echo str_replace('{&body}', $tmp, $html);
-} else echo str_replace('{&body}', "<p>Failed.</p>", $html);
+$file_name = $_POST['file_name'];
+$host_name = $_SERVER['HTTP_HOST'];
+if ($file_name == "") header("location:http://{$host_name}");
+else {
+	$file_path = "./.file/" . $file_name;
+	$text = $_POST['text'];
+	if ($text == "") {
+		echo "<script> alert('Content can not be empty.') </script>";
+		header("refresh:0;url=http://{$host_name}/{$file_name}"); 
+	} else {
+		if (!is_dir("./.file")) mkdir("./.file", 0755);
+		$newfile = fopen($file_path, "w");
+		fwrite($newfile, $text);
+		$html = str_combine(file('./base.html'));
+		if (file_exists($file_path)) {
+			$tmp = "<h2>Saved</h2>
+				  <a href='http://{$host_name}'><input type='button' title='goback' value='Home'/></a>";
+		    echo "<meta http-equiv='refresh' content='3; url=http://{$host_name}";
+			echo str_replace('{&body}', $tmp, $html);
+		} else echo str_replace('{&body}', "<p>Failed</p>", $html);
+	}
+}
+
 ?>
+
 
 
