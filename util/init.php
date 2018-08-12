c19d3889f0de202e6d4cb82f609b4d72dab4755b
lucienshui <18952904@qq.com>
2018-08-12 10:11:17 +0800

    fix init.php

--- util/init.php
+++ util/init.php
@@ -1,17 +1,14 @@
 <?php
-$config = require_once ('config.php');
+$config = require_once('config.php');
 $connection = mysqli_connect($config['dbhost'], $config['username'], $config['password']);
 if (!$connection) die('Error' . mysqli_error($connection));
-if ($connection->query("SELECT * FROM `clip`")) {
-    header("Refresh:0;url=/");
-} else {
-    $sqlSet = array(
-        "USE {$config['dbname']}",
-        "CREATE TABLE IF NOT EXISTS `clip` (
-            `keyword` varchar(107) PRIMARY KEY,
-	        `text` text)",
-    );
-    foreach ($sqlSet as $sql) if (!$connection->query($sql)) die('Error');
-    echo 'Success';
-}
+$sqlSet = array(
+    "USE {$config['dbname']}",
+    "CREATE TABLE IF NOT EXISTS `clip` (
+        `keyword` varchar(107) PRIMARY KEY,
+	    `text` text)",
+);
+foreach ($sqlSet as $sql) if (!$connection->query($sql)) die('Error');
+echo 'Success';
+unlink('init.php')
 ?>
