eaf5d53118be93fad82d5f955a6b8cb7e9fc1252
Lucien <lucien@lucien.ink>
2018-08-12 13:12:04 +0800

    Update init.php

--- util/init.php
+++ util/init.php
@@ -3,6 +3,7 @@ $config = require('config.php');
 $connection = mysqli_connect($config['dbhost'], $config['username'], $config['password']);
 if (!$connection) die('Error' . mysqli_error($connection));
 $sqlSet = array(
+    "CREATE DATABASE IF NOT EXISTS {$config['dbname']}",
     "USE {$config['dbname']}",
     "CREATE TABLE IF NOT EXISTS `clip` (
         `keyword` varchar(107) PRIMARY KEY,
